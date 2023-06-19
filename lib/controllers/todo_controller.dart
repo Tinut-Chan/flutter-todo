19784c7ea2ea1d3435f191e765fe9cbe3aac262a
Tinut-Chan <tinutchan@gmail.com>
2023-06-19 15:43:38 +0700

    search field update

--- lib/controllers/todo_controller.dart
+++ lib/controllers/todo_controller.dart
@@ -1,12 +1,27 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_todo/models/todo_model.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
 class TodoController extends GetxController {
+  final searchEditingController = TextEditingController().obs;
   var todos = List<Todo>.empty().obs;
+  // ConnectivityResult connectionStatus = ConnectivityResult.none;
+  // final Connectivity connectivity = Connectivity();
+  // var isConnected = false.obs;
+  // late StreamSubscription<ConnectivityResult> connectivitySubscription;
 
   @override
   void onInit() {
+    // connectivitySubscription =
+    //     Connectivity().onConnectivityChanged.listen((event) {
+    //   if (event == ConnectivityResult.wifi ||
+    //       event == ConnectivityResult.mobile) {
+    //     isConnected.value = true;
+    //   } else {
+    //     isConnected.value = false;
+    //   }
+    // });
     List? storedTodos = GetStorage().read<List>('todo');
     if (storedTodos != null) {
       todos = storedTodos.map((e) => Todo.fromJson(e)).toList().obs;
@@ -16,4 +31,45 @@ class TodoController extends GetxController {
     });
     super.onInit();
   }
+
+  final results = <Todo>[].obs;
+  void filterList(String searchText) {
+    if (searchText.isEmpty) {
+      results.value = todos;
+    } else {
+      results.value = todos
+          .where((item) => item.text!.toLowerCase().contains(
+                searchText.toLowerCase(),
+              ))
+          .toList();
+    }
+  }
+
+  // final todo = FirebaseDatabase.instance.reference();
+  // submitData() {
+  //   todo.child('items').add(items);
+  // }
+
+  // Future<void> initConnectivity() async {
+  //   late ConnectivityResult result;
+  //   // Platform messages may fail, so we use a try/catch PlatformException.
+  //   try {
+  //     result = await connectivity.checkConnectivity();
+  //   } on PlatformException catch (e) {
+  //     debugPrint('Couldn\'t check connectivity status, $e');
+  //     return;
+  //   }
+
+  //   return updateConnectionStatus(result);
+  // }
+
+  // Future<void> updateConnectionStatus(ConnectivityResult result) async {
+  //   connectionStatus = result;
+  // }
+
+  // @override
+  // void dispose() {
+  //   connectivitySubscription.cancel();
+  //   super.dispose();
+  // }
 }
